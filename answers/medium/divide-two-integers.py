332b39903490349e90d1538153bcdf58b7872ad2
Weiliang Li <To.be.impressive@gmail.com>
2016-03-05 20:16:15 +0800

    Add some answers of medium problems

---
+++ answers/medium/divide-two-integers.py
@@ -0,0 +1,43 @@
+class Solution:
+    # @param {int} dividend the dividend
+    # @param {int} divisor the divisor
+    # @return {int} the result
+    def divide(self, dividend, divisor):
+        # Write your code here
+        import math
+        MAX = 2147483647
+        a = dividend
+        b = divisor
+        if a == 0:
+            return 0
+        if b == 1:
+            return a
+        if a == b:
+            return 1
+        if b == 2 or b == 4 or b == 8 or b == 16:
+            return a >> int(math.log(b, 2))
+        if b == 0:
+            return MAX
+
+        positive = True
+        if a > 0 and b > 0:
+            a = a
+            b = b
+        elif a < 0 and b < 0:
+            a = -a
+            b = -b
+        elif a < 0 and b > 0:
+            a = -a
+            b = b
+            positive = False
+        else:
+            a = a
+            b = -b
+            positive = False
+        result = int(math.pow(2, math.log(a, 2)-math.log(b, 2)))
+        if result > MAX:
+            result = MAX
+        if positive:
+            return result
+        else:
+            return -result
---
+++ answers/medium/edit-distance.py
@@ -0,0 +1,24 @@
+class Solution:
+    # @param word1 & word2: Two string.
+    # @return: The minimum number of steps.
+    def minDistance(self, word1, word2):
+        # write your code here
+        if word1 == word2:
+            return 0
+        if len(word1) == 0:
+            return len(word2)
+        if len(word2) == 0:
+            return len(word1)
+        m, n = len(word1), len(word2)
+        d = [[0 for j in xrange(n+1)] for i in xrange(m+1)]
+        for i in xrange(0, m+1):
+            d[i][0] = i
+        for j in xrange(0, n+1):
+            d[0][j] = j
+        for i in xrange(1, m+1):
+            for j in xrange(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    d[i][j] = d[i-1][j-1]
+                else:
+                    d[i][j] = min(d[i-1][j], d[i][j-1], d[i-1][j-1]) + 1
+        return d[m][n]
