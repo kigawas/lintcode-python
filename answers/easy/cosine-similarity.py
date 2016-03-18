29987d746f36539d760e02a190f8cd62edd1543a
Weiliang Li <To.be.impressive@gmail.com>
2016-03-18 11:40:33 +0800

    Add cosine similarity

---
+++ answers/easy/cosine-similarity.py
@@ -0,0 +1,18 @@
+class Solution:
+    """
+    @param A: An integer array.
+    @param B: An integer array.
+    @return: Cosine similarity.
+    """
+    def cosineSimilarity(self, A, B):
+        # write your code here
+        if A is None or B is None or len(A) != len(B):
+            return 2.0000
+        s = 0
+        for a, b in zip(A, B):
+            s += a*b
+        sA = sum(map(lambda x: x*x, A))**0.5
+        sB = sum(map(lambda x: x*x, B))**0.5
+        if sA == 0 or sB == 0:
+            return 2.0000
+        return s/(sA*sB)
