73baf55eb73f5702c883dd2a5b98363951889440
Weiliang Li <To.be.impressive@gmail.com>
2016-03-23 13:38:42 +0800

    add remove duplicates in sorted array

---
+++ answers/easy/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    """
+    @param A: a list of integers
+    @return an integer
+    """
+    def removeDuplicates(self, A):
+        # write your code here
+        if A == []:
+            return 0
+        index = 0
+        for i in range(len(A)):
+            if A[index] != A[i]:
+                index += 1
+                A[index] = A[i]
+        return index+1
