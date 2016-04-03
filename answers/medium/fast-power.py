bece0fd6a2ffec1c1e60830ff8df908e4481b603
Weiliang Li <To.be.impressive@gmail.com>
2016-04-03 16:38:21 +0800

    Add fast-power

---
+++ answers/medium/fast-power.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    @param a, b, n: 32bit integers
+    @return: An integer
+    """
+
+    def fastPower(self, a, b, n):
+        if n == 1:
+            return a % b
+        elif n == 0:
+            return 1 % b
+        elif n < 0:
+            return -1
+
+        product = self.fastPower(a, b, n / 2)
+        product = (product * product) % b
+        if n % 2 == 1:
+            product = (product * a) % b
+
+        return product
